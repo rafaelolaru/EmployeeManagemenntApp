31100116f34df08c0941bc871915493360a34ebd
rafaelolaru <olarurafael1801@gmail.com>
2022-05-09 01:32:12 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module sef.app.employeemanagementapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens sef.app.employeemanagementapp to javafx.fxml;
+    exports sef.app.employeemanagementapp;
+}
