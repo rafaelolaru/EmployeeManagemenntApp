9b74309a962eb6f7dc6f2f68c60104f33a2dc4b2
Olah Antonio <antonioolah@yahoo.com>
2022-05-14 02:26:24 +0300

    Added Register

---
+++ src/main/java/org/loose/fis/registration/example/controllers/RegistrationController.java
@@ -0,0 +1,40 @@
+package org.loose.fis.registration.example.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.loose.fis.registration.example.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.registration.example.services.UserService;
+
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+    @FXML
+    private TextField fullNameField;
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Client", "Admin");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue(),fullNameField.getText(),phoneField.getText());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/loose/fis/registration/example/model/User.java
@@ -0,0 +1,106 @@
+package org.loose.fis.registration.example.model;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String role;
+    private String full_name;
+    private String phone;
+    private int salary=0,hours=0;
+
+    public User() {
+    }
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+
+    }
+    public User(String username, String password, String role,String full_name,String phone) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.full_name=full_name;
+        this.phone=phone;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFull_name(){return full_name;}
+
+    public void setFull_name(String full_name){this.full_name=full_name;}
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary(){return salary;}
+
+    public void setSalary(int salary){this.salary=salary;}
+
+    public int getHours(){return hours;}
+
+    public void setHours(int hours){this.hours=hours;}
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!username.equals(user.username)) return false;
+        if (!password.equals(user.password)) return false;
+        return role.equals(user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + role.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                ", full name='" + full_name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary='" + salary + '\'' +
+                ", hours='" + hours + '\'' +
+                '}';
+    }
+}
