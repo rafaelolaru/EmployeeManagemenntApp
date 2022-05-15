325bbb3fbc1abd422b32cb392b4cd5c0d79fbe3f
Olah Antonio <antonioolah@yahoo.com>
2022-05-15 20:49:39 +0300

    Added conditions for register

--- src/main/java/org/loose/fis/registration/example/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/registration/example/controllers/RegistrationController.java
@@ -1,17 +1,21 @@
 package org.loose.fis.registration.example.controllers;
 
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.loose.fis.registration.example.exceptions.UsernameAlreadyExistsException;
 import org.loose.fis.registration.example.services.UserService;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.function.UnaryOperator;
 
 public class RegistrationController {
 
@@ -33,20 +37,47 @@ public class RegistrationController {
         role.getItems().addAll("Client", "Admin");
     }
 
+
     @FXML
     public void handleRegisterAction(javafx.event.ActionEvent actionEvent) {
-        try {
-            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue(),fullNameField.getText(),phoneField.getText());
-            registrationMessage.setText("Account created successfully!");
-                Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
-                Scene scene = new Scene(root);
-                Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-                stage.setScene(scene);
-                stage.show();
-        } catch (Exception e) {
-            registrationMessage.setText(e.getMessage());
+        Alert alert=new Alert(Alert.AlertType.WARNING);
+        if(usernameField.getText().matches("  ")||usernameField.getText().equals(""))
+        {
+            alert.setContentText("Invalid username");
+            alert.show();
         }
-    }
+        else
+            if(passwordField.getText().length()<5)
+            {
+                alert.setContentText("Password too short");
+                alert.show();
+            }
+            else if(fullNameField.getText().equals(""))
+            {
+                alert.setContentText("Invalid name");
+                alert.show();
+            }
+            else if((!isNumeric(phoneField.getText()))||phoneField.getText().length()>=15||phoneField.getText().length()<7) {
+                alert.setContentText("Invalid phone number");
+                alert.show();
+            }
+            else if(role.getSelectionModel().isEmpty())
+            {
+                alert.setContentText("Please select a role");
+                alert.show();
+            }
+            else
+                try {
+                    UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue(), fullNameField.getText(), phoneField.getText());
+                    registrationMessage.setText("Account created successfully!");
+                    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+                    Scene scene = new Scene(root);
+                    Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (Exception e) {
+                    registrationMessage.setText(e.getMessage());}
+        }
 
     @FXML
     public void handleGoBackAction(javafx.event.ActionEvent actionEvent){
@@ -61,5 +92,10 @@ public class RegistrationController {
             registrationMessage.setText(e.getMessage());
         }
     }
+
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
+
 
 }
