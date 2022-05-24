6b5132fb719e71d2ebc49277a13abe0e3e86c768
Olah Antonio <antonioolah@yahoo.com>
2022-05-24 20:47:27 +0300

    Added backgrounds and icon

--- src/main/java/org/loose/fis/registration/example/Main.java
+++ src/main/java/org/loose/fis/registration/example/Main.java
@@ -4,15 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import org.loose.fis.registration.example.controllers.LogInController;
-import org.loose.fis.registration.example.model.User;
 import org.loose.fis.registration.example.services.UserService;
 
-import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
     private Stage primaryStage;
@@ -32,6 +28,10 @@ public class Main extends Application {
 
             Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
             primaryStage.setTitle("Employee Management");
+
+            Image icon = new Image(getClass().getResourceAsStream("/backgrounds/images/icon.png"));
+            primaryStage.getIcons().add(icon);
+
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
         }catch (Exception e){
