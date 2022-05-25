b7eb9a3925bb2be5f6cf4cbc096f795e626d65a9
rafaelolaru <olarurafael1801@gmail.com>
2022-05-25 14:10:14 +0300

    Added validation for TextFields

--- src/main/java/org/loose/fis/registration/example/controllers/GetSalariesController.java
+++ src/main/java/org/loose/fis/registration/example/controllers/GetSalariesController.java
@@ -1,6 +1,7 @@
 package org.loose.fis.registration.example.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.loose.fis.registration.example.model.Transaction;
@@ -44,22 +45,34 @@ public class GetSalariesController {
         salariesLabel.setText(String.valueOf(sum));
         salariesLabel.setVisible(true);
     }
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
 
     public void handleGetTotalMoney(){
-        float sum = 0;
-        int startDay,endDay,startMonth,endMonth;
-        startDay = Integer.parseInt(startDayTF.getText());
-        endDay = Integer.parseInt(endDayTF.getText());
-        startMonth = Integer.parseInt(startMonthTF.getText());
-        endMonth = Integer.parseInt(endMonthTF.getText());
-        for(Transaction transaction : transactions){
-            if(transaction.getDay() >= startDay && transaction.getDay() <= endDay &&
-                    transaction.getMonth() >= startMonth
-                    && transaction.getMonth() <= endMonth){
-                sum += transaction.getPayment();
+        Alert alert=new Alert(Alert.AlertType.WARNING);
+        if(isNumeric(startDayTF.getText()) && isNumeric(endDayTF.getText()) && isNumeric(startMonthTF.getText()) && isNumeric(endMonthTF.getText()))
+        {
+            float sum = 0;
+            int startDay,endDay,startMonth,endMonth;
+            startDay = Integer.parseInt(startDayTF.getText());
+            endDay = Integer.parseInt(endDayTF.getText());
+            startMonth = Integer.parseInt(startMonthTF.getText());
+            endMonth = Integer.parseInt(endMonthTF.getText());
+            for(Transaction transaction : transactions){
+                if(transaction.getDay() >= startDay && transaction.getDay() <= endDay &&
+                        transaction.getMonth() >= startMonth
+                        && transaction.getMonth() <= endMonth){
+                    sum += transaction.getPayment();
+                }
             }
+            salariesLabel.setText(String.valueOf(sum));
+            salariesLabel.setVisible(true);
         }
-        salariesLabel.setText(String.valueOf(sum));
-        salariesLabel.setVisible(true);
+        else{
+            alert.setContentText("Please enter only numbers.");
+            alert.show();
+        }
+
     }
 }
--- src/main/java/org/loose/fis/registration/example/controllers/TransactionMenuController.java
+++ src/main/java/org/loose/fis/registration/example/controllers/TransactionMenuController.java
@@ -1,10 +1,9 @@
 package org.loose.fis.registration.example.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import org.loose.fis.registration.example.exceptions.LunaInexistenta;
-import org.loose.fis.registration.example.exceptions.ZiInexistenta;
 import org.loose.fis.registration.example.model.Transaction;
 import org.loose.fis.registration.example.services.TransactionService;
 
@@ -22,13 +21,25 @@ public class TransactionMenuController {
     @FXML
     private CheckBox salaryCheckBox;
 
-    public void handleAddTransaction() throws LunaInexistenta, ZiInexistenta {
-        int payment = Integer.valueOf(sumTF.getText());
-        int day = Integer.parseInt(dayTF.getText());
-        int month = Integer.parseInt(monthTF.getText());
-        boolean salary = salaryCheckBox.isSelected();
-        System.out.println(String.valueOf(payment) + String.valueOf(day) + String.valueOf(month) + String.valueOf(salary));
-        Transaction transaction = new Transaction(payment,day,month,salary);
-        TransactionService.addTransaction(transaction);
+    public static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    public void handleAddTransaction() {
+        Alert alert=new Alert(Alert.AlertType.WARNING);
+        if (isNumeric(sumTF.getText()) && isNumeric(dayTF.getText()) && isNumeric(monthTF.getText())){
+
+            int payment = Integer.parseInt(sumTF.getText());
+            int day = Integer.parseInt(dayTF.getText());
+            int month = Integer.parseInt(monthTF.getText());
+            boolean salary = salaryCheckBox.isSelected();
+            System.out.println(String.valueOf(payment) + String.valueOf(day) + String.valueOf(month) + String.valueOf(salary));
+            Transaction transaction = new Transaction(payment, day, month, salary);
+            TransactionService.addTransaction(transaction);
+        }
+        else {
+            alert.setContentText("Please enter only numbers.");
+            alert.show();
+        }
     }
 }
